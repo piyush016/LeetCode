fdc7a24ff9fb8e3cd3e4e3eceb7d08e469db3a97
Piyush <57654154+piyush016@users.noreply.github.com>
2023-09-12 23:49:45 +0530

    Time: 0 ms (100%), Space: 43.2 MB (83.37%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,18 +9,18 @@
  *     }
  * }
  */
-public class Solution {adsfasdf
+public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null) // 0 or 1 node. Means linked list has no cycle
         return false;
 
         ListNode slow = head, fast = head;
 
-        while(fast != null && fast.next != null){ 
-        
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast)
+        while(fast != null && fast.next != null){ //If at any place fast or fast.next points to null that means linked list is straight
+
+            slow = slow.next; //slow moving 1
+            fast = fast.next.next; //fast moving 2
+            if(slow == fast) //if slow and fast gets equal that means it has a cycle.
             return true;
         }
         return false;
