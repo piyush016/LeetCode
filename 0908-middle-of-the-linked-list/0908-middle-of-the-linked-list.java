c5947fc0d167e43b556dc98b183d081075b7c5e1
Piyush <57654154+piyush016@users.noreply.github.com>
2023-09-19 23:43:14 +0530

    Time: 0 ms (100%), Space: 39.7 MB (75.16%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -8,17 +8,17 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-asdfasfd
+ // Slow fast pointer approach
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode fast = head, slow = head;
 
         while(fast.next != null){
-            if(fast.next.next == null){
+            if(fast.next.next == null){ // In even number list, they want ()n/2 + 1)th element
                 slow = slow.next;
                 break;
             }
+            //Otherwise normal traversal
             slow = slow.next;
             fast = fast.next.next;
         }
